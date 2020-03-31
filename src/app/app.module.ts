3c7a16cd464a27e987a9deb10cacb9775a2511be
afuentespas <afuentespas@gmail.com>
2020-03-31 20:53:23 +0200

    Contact Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { ModalModule } from 'ngx-bootstrap/modal'
 
 import { AppComponent } from './app.component';
 import { PersonalDataComponent } from './components/personal-data/personal-data.component';
@@ -31,7 +32,8 @@ import { AboutMeComponent } from './components/about-me/about-me.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ModalDirective } from 'ngx-bootstrap/modal';
+import contactIcons from '../../../assets/contact-icons.json';
 
 @Component({
   selector: 'cv-contacts',
@@ -7,10 +10,64 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
-  @Input() contacts: any;
+  @Input() contacts: any[];
 
-  constructor() { }
+  registerForm: FormGroup;
 
-  ngOnInit(): void { }
+  submitted: boolean = false;
+
+  contactIcons: any[];
+
+  @ViewChild(ModalDirective, { static: false }) modal: ModalDirective;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.contactIcons = contactIcons;
+    this.registerForm = this.formBuilder.group({
+      value: ['', Validators.required],
+      icon: ['', Validators.required],
+      index: ['']
+    });
+  }
+
+  get f() { 
+    return this.registerForm.controls; 
+  }
+
+  editContact(index: number): void {
+    this.registerForm.get('value').setValue(this.contacts[index].value);
+    this.registerForm.get('icon').setValue(this.contacts[index].icon);
+    this.registerForm.get('index').setValue(index);
+    this.modal.show();
+  }
+
+  deleteContact(index: number): void {
+    this.contacts.splice(index, 1);
+  }
+
+  clearForm(): void {
+    this.registerForm.reset();
+  }
+
+  closeModal(): void {
+    this.modal.hide();
+  }
+
+  onSubmit(): void{
+    this.submitted = true;
+    if (this.registerForm.valid) {
+      if(this.registerForm.get('index').value !== ''){
+        this.contacts[this.registerForm.get('index').value].value = this.registerForm.get('value').value;
+        this.contacts[this.registerForm.get('index').value].icon = this.registerForm.get('icon').value;
+      } else {
+        this.contacts.push({
+          value: this.registerForm.get('value').value,
+          icon: this.registerForm.get('icon').value
+        })
+      }
+    }
+    this.modal.hide();
+  }
 
 }
